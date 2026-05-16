9be794e6f9f638d39b5d275725428202bd22d29a
prateekarora7 <52380009+prateekarora7@users.noreply.github.com>
2026-05-16 17:36:22 +0530

    Initial commit

---
+++ src/test/java/com/catify/catify_api/CatifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catify.catify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
