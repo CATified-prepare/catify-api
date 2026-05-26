94d7625ec4cbc57f65f69d708d874ae5a2f94fb2
GroverVansh <vanshgrover14.vg@gmail.com>
2026-05-26 22:51:41 +0200

    US-004 adding document in qdrant

--- src/main/java/com/catify/catify_api/config/SecurityConfig.java
+++ src/main/java/com/catify/catify_api/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/v1/**").permitAll()
                 .requestMatchers("/actuator/health").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
             );
         return http.build();
     }
